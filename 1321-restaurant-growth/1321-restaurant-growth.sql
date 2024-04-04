11c6b0971e3a6705b61af8b735265b18b79a1d09
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-04-05 00:45:05 +0530

    Time: 590 ms (87.11%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,7 +1,16 @@
-# Write your MySQL query statement below
+# # Write your MySQL query statement below
 
-select b.visited_on, sum(a.amount) as amount, round(sum(a.amount)/7,2) as average_amount
-from Customer a, (select distinct visited_on from Customer) b
-where DATEDIFF(b.visited_on, a.visited_on) between 0 and 6
-group by b.visited_on
-having count(distinct a.visited_on)=7
+# select b.visited_on, sum(a.amount) as amount, round(sum(a.amount)/7,2) as average_amount
+# from Customer a, (select distinct visited_on from Customer) b
+# where DATEDIFF(b.visited_on, a.visited_on) between 0 and 6
+# group by b.visited_on
+# having count(distinct a.visited_on)=7
+# Write your MySQL query statement below
+SELECT visited_on, amount, ROUND(amount/7, 2) average_amount
+FROM (
+    SELECT DISTINCT visited_on, 
+    SUM(amount) OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY   PRECEDING AND CURRENT ROW) amount, 
+    MIN(visited_on) OVER() 1st_date 
+    FROM Customer
+) t
+WHERE visited_on>= 1st_date+6;
