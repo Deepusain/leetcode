b76ce67bb32bf47adcff5594a4ae0ea2f4275279
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-04-09 10:36:55 +0530

    Time: 4 ms (41.19%), Space: 9.4 MB (46.11%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int x =   tickets[k];
@@ -18,5 +18,33 @@ public:
             }
         }
         return count;
+    }
+};*/
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int time = 0;
+
+        // If person k only needs one ticket, return the time to buy it
+        if (tickets[k] == 1)
+            return k + 1;
+
+        // Continue buying tickets until person k buys all their tickets
+        while (tickets[k] > 0) {
+            for (int i = 0; i < n; i++) {
+                // Buy a ticket at index 'i' if available
+                if (tickets[i] != 0) {
+                    tickets[i]--;
+                    time++;
+                }
+
+                // If person k bought all their rickets, return the time
+                if (tickets[k] == 0)
+                    return time;
+            }
+        }
+
+        return time;
     }
 };
