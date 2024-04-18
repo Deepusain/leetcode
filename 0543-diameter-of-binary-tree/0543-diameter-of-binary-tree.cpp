df5aefe17dd2452d6c6665d220f9556d92bcd578
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-04-18 20:35:44 +0530

    Time: 14 ms (20.69%), Space: 22.2 MB (12.74%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,25 +12,16 @@
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        // Define a helper function to calculate the diameter recursively
-        int res = 0;
-        diameter(root, res);
-        return res;
+        int d=0;
+        rec(root, d);
+        return d;
     }
-
-private:
-    int diameter(TreeNode* curr, int& res){
-        // Base case: if the current node is null, return 0
-        if (!curr) return 0;
-        
-        // Recursively calculate the diameter of left and right subtrees
-        int left = diameter(curr->left, res);
-        int right = diameter(curr->right, res);
-
-        // Update the maximum diameter encountered so far
-        res = std::max(res, left + right);
-        
-        // Return the depth of the current node
-        return std::max(left, right) + 1;
+    
+    int rec(TreeNode* root, int &d) {
+        if(root == NULL) return 0;
+        int ld = rec(root->left, d);
+        int rd = rec(root->right, d);
+        d=max(d,ld+rd);
+        return max(ld,rd)+1;
     }
 };
