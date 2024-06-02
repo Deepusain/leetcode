0237dd4a7ae663db12f6b682b86eabe6a8279285
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-06-02 13:32:28 +0530

    Time: 14 ms (82.25%), Space: 27 MB (60.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,17 +1,13 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        n=len(s)
-        n0=n//2
-        for i in range(n0):
-            tmp=s[i]
-            s[i]=s[n-1-i]
-            s[n-1-i]=tmp
-# class Solution:
-#     def reverseString(self, s: List[str]) -> None:
-#         """
-#         Do not return anything, modify s in-place instead.
-#         """
-        
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;j--;
+        }
+    }
+};
