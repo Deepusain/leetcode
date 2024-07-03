12bf3961e66801bb4f4d71b99c8915a26fd33f5b
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-07-03 13:55:21 +0530

    Time: 76 ms (21.09%), Space: 38.8 MB (50.84%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -12,18 +12,3 @@ public:
         return ans;
     }
 };
-// class Solution {
-// public:
-//     int minDifference(vector<int>& nums) {
-//         if(nums.size() <= 4) {
-//             return 0;
-//         }
-//         sort(nums.begin() , nums.end());
-//         int i=0;
-//         while(i<3){
-//             nums[nums.size()-1-i] =nums[0];
-//             i++;
-//         }
-//         return nums[nums.size()-1-i]-nums[0];
-//     }
-// };
