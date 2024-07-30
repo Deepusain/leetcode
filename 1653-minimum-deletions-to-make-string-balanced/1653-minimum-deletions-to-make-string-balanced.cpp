14abe2ea4dece4e3089f108aecd93c01843c74e1
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-07-30 12:29:27 +0530

    Time: 76 ms (89.95%), Space: 23.5 MB (80.38%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,10 +1,15 @@
-class Solution:
-    def minimumDeletions(self, s: str) -> int:
-        bCount = 0
-        minDeletions = 0
-        for char in s:
-            if char == 'a':
-                minDeletions = min(minDeletions + 1, bCount)
-            else:
-                bCount += 1   
-        return minDeletions
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bCount = 0;
+        int minDeletions = 0;
+        for (int i =0;i<s.length();i++){
+            if (s[i] == 'a'){
+                minDeletions = min(minDeletions + 1, bCount);
+                }else{
+                bCount ++;
+                }
+        }  
+        return minDeletions;
+    }
+};
