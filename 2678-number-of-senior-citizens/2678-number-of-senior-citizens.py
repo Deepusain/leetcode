834d5109e68f09b3338021ebe6a40d834a4d2038
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-08-01 11:15:44 +0530

    Time: 50 ms (33.08%), Space: 16.6 MB (32.17%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -2,6 +2,8 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         ans =0
         for i in details:
+            # int(string) s to i formula
+            # string[11:13] substring from 11 to 12
             if int(i[11:13])>60:
                 ans+=1
         return ans
