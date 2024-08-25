a3c60ae2a83819f8e4d099f1e7ce0421258da4ee
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-08-25 09:35:05 +0530

    Time: 4 ms (32.22%), Space: 9.7 MB (96.11%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,45 +11,47 @@
  */
 class Solution {
 public:
-    // Recursive function for postorder traversal
-void postorder(TreeNode* root, vector<int>& result) {
-    if (root == nullptr) {
-        return;
-    }
-    // Traverse the left subtree
-    postorder(root->left, result);
-    // Traverse the right subtree
-    postorder(root->right, result);
-    // Process the root node
-    result.push_back(root->val);
-}
-    
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> result;
-    postorder(root, result);
-    return result;
-
-        // if(root == NULL) return {};
-        // vector<int>postorder;
-        // stack<TreeNode*>s;
-        // s.push(root);
-        // while(!s.empty()){
-        //     TreeNode* curr = s.top();
-        //     if(curr->left){
-        //         s.push(curr->left);
-        //         curr->left = NULL;
-        //     }
-        //     else{
-        //         if(curr->right){
-        //             s.push(curr->right);
-        //             curr->right = NULL;
-        //         }
-        //         else{
-        //             postorder.push_back(curr->val);
-        //             s.pop();
-        //         }
-        //     }
-        // }
-        // return postorder;
+        if(root == NULL) return {};
+        vector<int>postorder;
+        stack<TreeNode*>s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* curr = s.top();
+            if(curr->left){
+                s.push(curr->left);
+                curr->left = NULL;
+            }
+            else{
+                if(curr->right){
+                    s.push(curr->right);
+                    curr->right = NULL;
+                }
+                else{
+                    postorder.push_back(curr->val);
+                    s.pop();
+                }
+            }
+        }
+        return postorder;
     }
+//     // Recursive Approx
+//     // Recursive function for postorder traversal
+//     void postorder(TreeNode* root, vector<int>& result) {
+//         if (root == nullptr) {
+//             return;
+//         }
+//         // Traverse the left subtree
+//         postorder(root->left, result);
+//         // Traverse the right subtree
+//         postorder(root->right, result);
+//         // Process the root node
+//         result.push_back(root->val);
+//     }
+    
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         postorder(root, result);
+//         return result;
+//     }
 };
