6f4eab4eefd982bd0f39041172d079d766da5658
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-09-04 13:40:53 +0530

    Time: 106 ms (35.64%), Space: 39.6 MB (65.75%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,68 +1,32 @@
-// class Solution {
-// public:
-//     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-//         unordered_set<string> obs;
-//         for(auto& obstacle : obstacles){
-//             obs.insert(to_string(obstacle[0])+","+to_string(obstacle[1]));
-//         }
-//         int x=0,y=0,dx=0,dy=1;
-//         int ans=0;
-//         for(int c :commands){
-//             if(c==-1){
-//                 int temp = dx;
-//                 dx=dy;
-//                 dy=-temp;
-//             }else if(c==-1){
-//                 int temp = dx;
-//                 dx=-dy;
-//                 dy=temp;
-//             }else{
-//                 for(int i=0 ; i<c;++i){
-//                     int xx = x+dx;
-//                     int yy = y+dy;
-//                     if(obs.find(to_string(xx)+","+to_string(yy)) != obs.end()) break;
-//                     x = xx;
-//                     y = yy;
-//                     ans = max(ans, x*x+y*y);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-       
         unordered_set<string> obs;
-        for (auto& obstacle : obstacles) {
-            obs.insert(to_string(obstacle[0]) + "," + to_string(obstacle[1]));
+        for(auto& obstacle : obstacles){
+            obs.insert(to_string(obstacle[0])+","+to_string(obstacle[1]));
         }
-
-        int x = 0, y = 0, dx = 0, dy = 1;
-        int ans = 0;
-
-        for (int c : commands) {
-            if (c == -1) { 
+        int x=0,y=0,dx=0,dy=1;
+        int ans=0;
+        for(int c :commands){
+            if(c==-1){
                 int temp = dx;
-                dx = dy; dy = -temp;
-            } else if (c == -2) { 
+                dx=dy;
+                dy=-temp;
+            }else if(c==-2){
                 int temp = dx;
-                dx = -dy; dy = temp;
-            } else {
-  
-                for (int i = 0; i < c; ++i) {
-                    int xx = x + dx;
-                    int yy = y + dy;
-
-                    if (obs.find(to_string(xx) + "," + to_string(yy)) != obs.end()) break; 
+                dx=-dy;
+                dy=temp;
+            }else{
+                for(int i=0 ; i<c;++i){
+                    int xx = x+dx;
+                    int yy = y+dy;
+                    if(obs.find(to_string(xx)+","+to_string(yy)) != obs.end()) break;
                     x = xx;
                     y = yy;
-                    ans = max(ans, x * x + y * y); 
+                    ans = max(ans, x*x+y*y);
                 }
             }
         }
-
         return ans;
     }
 };
