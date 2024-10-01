800e32afeee3dabb88e4174b3a9a6bd1a5eadb8b
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2024-10-01 17:25:23 +0530

    Time: 89 ms (80.82%), Space: 64.3 MB (94.58%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,42 @@
+// class Solution {
+// public:
+//     bool canArrange(vector<int>& arr, int k) {
+//         vector<int>mp(k,0);
+//         for(auto&i: arr){
+//             int rem = (i%k+k)%k;
+//             mp[rem]++;
+//         }
+//         if(mp[0]%2 !=0) return false;
+        
+//         for (int i =1;i<k/2;i++){
+//             int counterHalf = k-i;
+//             if(mp[i] != mp[counterHalf])return false;
+//         }
+//         return true;
+//     }
+// };
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> freq(k, 0);
+        for (int num : arr) {
+            int rem = num % k;
+            if (rem < 0) {
+                rem += k;
+            }
+            freq[rem]++;
+        }
+        
+        if (freq[0] % 2 != 0) {
+            return false;
+        }
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
