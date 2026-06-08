001cf35cbabee881bc3d398c0539543a159ec1e2
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-06-08 11:33:18 +0530

    Time: 11 ms (30.45%) | Memory: 133.5 MB (43.07%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         int n=nums.size();
+//         vector<int> res(n);
+//         int left=0 , right=n-1;
+//         for(int i=0,j=n-1;i<n;i++,j--){
+//             if(nums[i]<pivot){
+//                 res[left]=nums[i];
+//                 left++;
+//             }
+//             if(nums[j]>pivot){
+//                 res[right]=nums[j];
+//                 right--;
+//             }
+//         }
+//         while(left<=right){
+//             res[left]=pivot;
+//             left++;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n=nums.size();
-        vector<int> res(n);
-        int left=0 , right=n-1;
-        for(int i=0,j=n-1;i<n;i++,j--){
-            if(nums[i]<pivot){
-                res[left]=nums[i];
-                left++;
-            }
-            if(nums[j]>pivot){
-                res[right]=nums[j];
-                right--;
-            }
+        vector<int>ans ;
+        for(auto x : nums)
+        {
+            if(x < pivot)
+            ans.push_back(x);
         }
-        while(left<=right){
-            res[left]=pivot;
-            left++;
+        for(auto x : nums)
+        {
+            if(x == pivot)
+            ans.push_back(x);
         }
-        return res;
+        for(auto x : nums)
+        {
+            if(x >pivot)
+            ans.push_back(x);
+        }
+        return ans;
     }
 };
