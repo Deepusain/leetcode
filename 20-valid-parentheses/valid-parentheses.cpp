d888ebdc2dba0968cb003ac64d1514d95e421ce7
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-06-09 13:56:05 +0530

    Time: 1 ms (18.35%) | Memory: 9.1 MB (19.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stack;
+        for(char c:s){
+            if(c=='(' ||c=='{' ||c=='['){
+                stack.push(c);
+            }else{
+                if(stack.empty() || (c==')' && stack.top() !='(' ) || (c=='}' && stack.top() !='{') || (c==']' && stack.top() !='[') ){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+};
