8d1afe31666bf45df14e94534815375a9bf16743
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-06-09 09:58:03 +0530

    Time: 1 ms (48.64%) | Memory: 103.6 MB (96.89%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -1,14 +1,43 @@
+// class Solution {
+// public:
+//     long long maxTotalValue(vector<int>& nums, int k) {
+//         int n = nums.size();
+        
+//         // 1. Pehle element se min aur max ko initialize karein
+//         int min_val = INT_MAX;
+//         int max_val = INT_MIN;
+        
+//         // 2. Single loop: Index 1 se end tak sirf ek baar scan karein
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] < min_val) {
+//                 min_val = nums[i];
+//             }
+//             if (nums[i] > max_val) {
+//                 max_val = nums[i];
+//             }
+//         }
+        
+//         // 3. Best single subarray ki value nikaalo
+//         long long single_max_value = max_val - min_val;
+        
+//         // 4. Answer return karein
+//         return single_max_value * k;
+
+//     }
+// };
+
+
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int n = nums.size();
         
         // 1. Pehle element se min aur max ko initialize karein
-        int min_val = INT_MAX;
-        int max_val = INT_MIN;
+        int min_val = nums[0];
+        int max_val = nums[0];
         
         // 2. Single loop: Index 1 se end tak sirf ek baar scan karein
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (nums[i] < min_val) {
                 min_val = nums[i];
             }
