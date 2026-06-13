eaea883fca600134866e1dbb9beddcee62af227e
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-06-13 11:56:24 +0530

    Time: 2 ms (32.35%) | Memory: 43.8 MB (24.57%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        string res = ""; // Vector ki jagah simple string banayi
+        string res = ""; 
         char alphabet[] = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
         
         for(string word : words){
@@ -9,9 +9,8 @@ public:
             for(char c : word){
                 sum += weights[c - 'a'];
             }
-            // string mein character ko '+' se directly joda (append) ja sakta hai
             res += alphabet[sum % 26]; 
         }
-        return res; // Ab return type 'string' se match karega
+        return res; 
     }
 };
