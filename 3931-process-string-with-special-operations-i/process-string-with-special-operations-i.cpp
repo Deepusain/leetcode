edc8297dcf9b06311aec1199a7aea2a2d9888a2c
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-06-16 11:56:56 +0530

    Time: 3 ms (76.89%) | Memory: 39.8 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res ="";
+        for(auto& c : s){
+            if(c == '*'){
+                if (!res.empty()) {
+                    res.pop_back(); 
+                }
+            }else if(c == '#'){
+                res+=res;
+            }else if(c=='%'){
+                reverse(res.begin(), res.end());
+            }else{
+                res+=c;
+            }
+        }
+        return res;
+    }
+};
