f1353befa104a2c195f54c233349b36a93a58e4f
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-07-07 22:56:15 +0530

    Time: 2 ms (9.92%) | Memory: 8.9 MB (89.09%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,16 +1,40 @@
+// class Solution {
+// public:
+//     long long sumAndMultiply(int n) {
+//         string s= to_string(n);
+//         string x ="";
+//         long long sum=0;
+//         for(auto &c : s){
+//             if(c !='0'){
+//                 x+=c;
+//                 sum += (c - '0');
+//             }
+//         }
+//         if (x.empty()) return 0; 
+//         return stoll(x) * sum;
+//     }
+// };
+
+
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        string s= to_string(n);
-        string x ="";
-        int sum=0;
-        for(auto &c : s){
-            if(c !='0'){
-                x+=c;
-                sum += (c - '0');
+        long long x = 0;
+        long long sum = 0;
+        long long multiplier = 1;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            
+            if (digit != 0) {
+                x += digit * multiplier;
+                sum += digit;
+                multiplier *= 10;
             }
+            
+            n /= 10;
         }
-        if (x.empty()) return 0; 
-        return stoll(x) * sum;
+        
+        return x * sum;
     }
 };
