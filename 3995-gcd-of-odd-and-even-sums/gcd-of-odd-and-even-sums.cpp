678014ad80fa13d6f0b8911f24f5adf2736e7abd
Deepak Sain <135960041+Deepusain@users.noreply.github.com>
2026-07-16 00:51:38 +0530

    Time: 4 ms (13.18%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) {
-        return n;
+    int gcdOfOddEvenSums(int n) 
+    {
+        int so=0,se=0;
+        for(int i=1;i<=n*2;i++)
+        {
+            if(i%2==0) se+=i;
+            else so+=i;
+        }
+        int y=0;
+        for(int i=1;i<=n*2;i++)
+        {
+            if(so%i==0 && se%i==0) y=max(y,i);
+        }
+        return y;
     }
 };
+
+
+// class Solution {
+// public:
+//     int gcdOfOddEvenSums(int n) {
+//         return n;
+//     }
+// };
